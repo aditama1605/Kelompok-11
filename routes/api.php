4e2bd586770cad2bae0de334d3c59c416eda7250
Mu-jikri <muhammadzikrifisika@gmail.com>
2025-05-07 13:29:47 +0700

    Laporan Perkembangan

--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,14 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Api\LaporanPerkembanganController;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+
+Route::get('laporan-perkembangan', [LaporanPerkembanganController::class, 'index']);
+Route::get('laporan-perkembangan/{id}', [LaporanPerkembanganController::class, 'show']);
+Route::post('laporan-perkembangan', [LaporanPerkembanganController::class, 'store']);
+Route::put('laporan-perkembangan/{id}', [LaporanPerkembanganController::class, 'update']);
+Route::delete('laporan-perkembangan/{id}', [LaporanPerkembanganController::class, 'destroy']);
