5853928b83dc0243c49fc56325930335f11c5ea7
AlbiMst <albimustakim61@gmail.com>
2025-05-07 12:07:19 +0700

    table user

---
+++ app/Http/Controllers/Api/UserController.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
+
+class UserController extends Controller
+{
+    public function index()
+    {
+        return User::all();
+    }
+
+    public function store(Request $request)
+    {
+        $request->validate([
+            'nama' => 'required',
+            'email' => 'required|email|unique:user,email',
+            'password' => 'required',
+            'role' => 'required|in:pasien,terapis',
+        ]);
+
+        $user = User::create([
+            'nama' => $request->nama,
+            'email' => $request->email,
+            'password' => Hash::make($request->password),
+            'role' => $request->role,
+            'jadwal_terapi_id_jadwal_terapi' => $request->jadwal_terapi_id_jadwal_terapi
+        ]);
+
+        return response()->json($user, 201);
+    }
+
+    public function show($id)
+    {
+        return User::findOrFail($id);
+    }
+
+    public function update(Request $request, $id)
+    {
+        $user = User::findOrFail($id);
+
+        $user->update($request->only(['nama', 'email', 'role', 'jadwal_terapi_id_jadwal_terapi']));
+
+        return response()->json($user);
+    }
+
+    public function destroy($id)
+    {
+        User::destroy($id);
+        return response()->json(['message' => 'User deleted']);
+    }
+}
